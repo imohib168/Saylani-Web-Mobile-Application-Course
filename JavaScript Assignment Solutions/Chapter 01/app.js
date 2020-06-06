2dff19ebaad8e05e0c40f3de79e2587e2612dc63
Mohib Ismail <imohib168@gmail.com>
2020-06-06 22:15:20 +0500

    JS Assignment Solutions

--- JavaScript Assignment Solutions/Chapter 01/app.js
+++ JavaScript Assignment Solutions/Chapter 01/app.js
@@ -1,21 +1,21 @@
 
 
 // Task: 01
-alert("\n Task: 01 \n\n Hello Users! Welcome to My Website...")
+alert("\n Task: 01 \n\n Hello Users! Welcome to My Website...");
 
 
 // Task: 02
-alert("\n Task: 02 \n\n Error! Please enter a Valid password")
+alert("\n Task: 02 \n\n Error! Please enter a Valid password");
 
 
 // Task: 03
-alert("\n Task: 03 \n\n welcome to JS land... \n Happy Coding!")
+alert("\n Task: 03 \n\n welcome to JS land... \n Happy Coding!");
 
 
 // Task: 04
-alert("\n Task: 04 \n Welcome to JS Land...")
-alert("\n Task: 04 \n Happy Coding! \n\n prevent this message from creating additional dialogs")
+alert("\n Task: 04 \n Welcome to JS Land...");
+alert("\n Task: 04 \n Happy Coding! \n\n prevent this message from creating additional dialogs");
 
 
 // Task: 05
-console.log("Hello... I can run JS through through my web browser's console")
+console.log("Hello... I can run JS through through my web browser's console");
