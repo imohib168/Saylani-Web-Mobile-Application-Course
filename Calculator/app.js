b1f64b3767a3ead4793fded767d51c56e8bb0108
Mohib Ismail <imohib168@gmail.com>
2020-06-28 15:08:26 +0500

    Assignment 05: Simple Calculator

--- Calculator/app.js
+++ Calculator/app.js
@@ -12,3 +12,9 @@ function finalresult() {
     var res = document.getElementById("output-result");
     res.value = eval(res.value)
 }
+
+function backspace(){
+    var res = document.getElementById("output-result");
+    var backspaced = res.value.slice(0, res.value.length - 1);
+    res.value = backspaced
+}
